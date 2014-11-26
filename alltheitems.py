ad5d76d82acec8fe06eec8a8f3bfb9bcfd12a70f
Fenhl <fenhl@fenhl.net>
2014-11-26 22:08:24 +0000

    Make the website a bottle app

---
+++ alltheitems.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Wurstmineberg: All The Items
+"""
+
+import bottle
+
+application = bottle.Bottle()
+document_root = '/var/www/alltheitems.wurstmineberg.de'
+
+@application.route('/')
+def show_index():
+    """The index page."""
+    return bottle.static_file('index.html', root=document_root)
+
+@application.route('/alltheitems.png')
+def image_alltheitems():
+    """The “Craft ALL the items!” image."""
+    return bottle.static_file('alltheitems.png', root=document_root)
+
+if __name__ == '__main__':
+    bottle.run(app=application, host='0.0.0.0', port=8081)
