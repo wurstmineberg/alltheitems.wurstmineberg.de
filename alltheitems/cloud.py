0b7f7e19edc689640f81cddf41592259b3b9b958
Fenhl <fenhl@fenhl.net>
2015-08-09 08:17:02 +0000

    Fix Cloud table background color explanation code

--- alltheitems/cloud.py
+++ alltheitems/cloud.py
@@ -12,59 +12,49 @@ def chest_iter():
             for z, chest in enumerate(corridor):
                 yield x, corridor, y, floor, z, chest
 
-def image_from_chest(cloud_chest):
+def chest_background_color(cloud_chest):
     exists = cloud_chest.get('exists', True)
     has_smart_chest = cloud_chest.get('hasSmartChest', True)
     has_sorter = cloud_chest.get('hasSorter', exists)
     has_overflow = cloud_chest.get('hasOverflow', exists)
     stackable = ati.item_info_from_stub(cloud_chest).get('stackable', True)
     if stackable and has_sorter and not has_overflow:
-        background_color = '#f00'
+        return '#f00'
     elif not exists:
-        background_color = '#777'
+        return '#777'
     elif not has_smart_chest:
-        background_color = '#f70'
+        return '#f70'
     elif not stackable:
-        background_color = '#0ff'
+        return '#0ff'
     elif not has_sorter:
-        background_color = '#ff0'
+        return '#ff0'
     else:
-        background_color = 'transparent'
-    return '<td style="background-color: {};">{}</td>'.format(background_color, ati.item_stub_image(cloud_chest))
+        return 'transparent'
+
+def image_from_chest(cloud_chest):
+    return '<td style="background-color: {};">{}</td>'.format(chest_background_color(cloud_chest), ati.item_stub_image(cloud_chest))
 
 def index():
     yield ati.header(title='Cloud')
     def body():
         yield '<p>The <a href="http://wiki.{host}/Cloud">Cloud</a> is the public item storage on <a href="http://{host}/">Wurstmineberg</a>, consisting of 6 underground floors with <a href="http://wiki.{host}/SmartChest">SmartChests</a> in them.</p>'.format(host=ati.host)
+        color_explanations = {
+            '#f00': '<p>A red background means that the chest has a sorter but the SmartChest and/or the overflow is missing. This can break other SmartChests, so it should be fixed as soon as possible!</p>',
+            '#777': "<p>A gray background means that the chest hasn't been built yet or is still located somewhere else.</p>",
+            '#f70': "<p>An orange background means that the chest doesn't have a SmartChest yet. It can only store 54 stacks.</p>",
+            '#0ff': '<p>A cyan background means that the chest has no sorter because it stores an unstackable item. These items should not be automatically <a href="http://wiki.wurstmineberg.de/Soup#Cloud">sent</a> to the Cloud.</p>',
+            '#ff0': "<p>A yellow background means that the chest doesn't have a sorter yet.</p>",
+            'transparent': '<p>A white background means that everything is okay: the chest has a SmartChest, a sorter, and overflow protection.</p>'
+        }
         explained_colors = set()
         for _, _, _, _, _, chest in chest_iter():
-            if not chest.get('exists', True):
-                if 'gray' not in explained_colors:
-                    yield "<p>A gray background means that the chest hasn't been built yet or is still located somewhere else.</p>"
-                    explained_colors.add('gray')
-            elif chest['hasSorter']:
-                if chest['hasOverflow'] and chest['hasSmartChest']:
-                    explained_colors.add('white')
-                else:
-                    if 'red' not in explained_colors:
-                        yield '<p>A red background means that the chest has a sorter but the SmartChest and/or the overflow is missing. This can break other SmartChests, so it should be fixed as soon as possible!</p>'
-                        explained_colors.add('red')
-            elif chest['hasSmartChest']:
-                stackable = ati.item_info_from_stub(chest).get('stackable', True)
-                if stackable is True or stackable > 1:
-                    if 'yellow' not in explained_colors:
-                        yield "<p>A yellow background means that the chest doesn't have a sorter yet.</p>"
-                        explained_colors.add('yellow')
-                else:
-                    if 'cyan' not in explained_colors:
-                        yield '<p>A cyan background means that the chest has no sorter because it stores an unstackable item. These items should not be automatically <a href="http://wiki.wurstmineberg.de/Soup#Cloud">sent</a> to the Cloud.</p>'
-                        explained_colors.add('cyan')
-            else:
-                if 'orange' not in explained_colors:
-                    yield "<p>An orange background means that the chest doesn't have a SmartChest yet. It can only store 54 stacks.</p>"
-                    explained_colors.add('orange')
-        if 'white' in explained_colors and len(explained_colors) > 1:
-            yield '<p>A white background means that everything is okay: the chest has a SmartChest, a sorter, and overflow protection.</p>'
+            chest_color = chest_background_color(chest)
+            if chest_color not in explained_colors:
+                if chest_color != 'transparent':
+                    yield color_explanations[chest_color]
+                explained_colors.add(chest_color)
+        if 'transparent' in explained_colors and len(explained_colors) > 1:
+            yield color_explanations['transparent']
         yield """<style type="text/css">
             .item-table td {
                 box-sizing: content-box;
