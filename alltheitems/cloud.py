96a40a858b7d6d50a076feec5458e165726a849b
Fenhl <fenhl@fenhl.net>
2015-11-05 03:08:54 +0000

    Fix syntax error in cloud.py

--- alltheitems/cloud.py
+++ alltheitems/cloud.py
@@ -772,7 +772,7 @@ def index():
             ('orange', "<p>An orange background means that the chest doesn't have a SmartChest yet. It can only store 54 stacks.</p>"),
             ('yellow', "<p>A yellow background means that the chest doesn't have a sorter yet.</p>"),
             ('cyan', '<p>A cyan background means that the chest has no sorter because it stores an unstackable item. These items should not be automatically <a href="http://wiki.wurstmineberg.de/Soup#Cloud">sent</a> to the Cloud.</p>'),
-            (None, '<p>A white background means that everything is okay: the chest has a SmartChest, a sorter, and overflow protection.</p>)'
+            (None, '<p>A white background means that everything is okay: the chest has a SmartChest, a sorter, and overflow protection.</p>)')
         ])
         for chest_color in colors_to_explain:
             if chest_color is not None or len(colors_to_explain) > 1:
