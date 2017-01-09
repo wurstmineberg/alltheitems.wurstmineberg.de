f14b9d2bf3a2f977a4969cfbdaaf3518d93242a3
Fenhl <fenhl@fenhl.net>
2017-01-09 10:16:56 +0000

    obtaining: Render craftingShapeless

--- alltheitems/obtaining.py
+++ alltheitems/obtaining.py
@@ -22,6 +22,18 @@ def crafting_shaped(i, item_info, method, **kwargs):
         %end
     """, chunked=more_itertools.chunked, i=i, inventory_table=alltheitems.util.inventory_table, item_info=item_info, method=method)
 
+@method('craftingShapeless')
+def crafting_shapeless(i, item_info, method, **kwargs):
+    if len(method['recipe']) == 9 and all(l == r for l, r in more_itertools.stagger(method['recipe'], offsets=(0, 1))):
+        return crafting_shaped(i, item_info, method, **kwargs)
+    return bottle.template("""
+        <p>{{item_info['name']}} can {{'' if i == 0 else 'also '}}be crafted using the following shapeless recipe:</p>
+        {{!inventory_table([method['recipe']], style={'margin-left': 'auto', 'margin-right': 'auto'})}}
+        %if method.get('outputAmount', 1) > 1:
+            <p>This will create {{method['outputAmount']}} items per crafting process.</p>
+        %end
+    """, i=i, inventory_table=alltheitems.util.inventory_table, item_info=item_info, method=method)
+
 def render(**kwargs):
     method_type = kwargs['method']['type']
     if method_type in METHODS:
