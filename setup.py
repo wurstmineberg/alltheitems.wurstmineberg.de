52b1e53fb743c8014ad35eccff50ab09a1e00b1c
Fenhl <fenhl@fenhl.net>
2017-06-19 12:38:09 +0000

    Add dependencies to setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python
 
-from setuptools import setup
+import setuptools
 
-setup(name='alltheitems.wurstmineberg.de',
-      description='a searchable web frontend for items.json',
-      author='Wurstmineberg',
-      author_email='mail@wurstmineberg.de',
-      packages=["alltheitems"],
-      use_scm_version = {
-            "write_to": "alltheitems/_version.py",
-          },
-      setup_requires=["setuptools_scm"],
-      package_data={"alltheitems": ["assets/*.json"]}
-     )
-
-
+setuptools.setup(
+    name='alltheitems.wurstmineberg.de',
+    description='a searchable web frontend for items.json',
+    author='Wurstmineberg',
+    author_email='mail@wurstmineberg.de',
+    packages=['alltheitems'],
+    use_scm_version = {
+        'write_to': 'alltheitems/_version.py',
+    },
+    setup_requires=['setuptools_scm'],
+    package_data={'alltheitems': ['assets/*.json']},
+    install_requires=[
+        'api',
+        'bottle',
+        'minecraft',
+        'more-itertools',
+        'wmb'
+    ],
+    dependency_links=[
+        'git+https://github.com/wurstmineberg/api.wurstmineberg.de.git#egg=api',
+        'git+https://github.com/wurstmineberg/systemd-minecraft.git#egg=minecraft',
+        'git+https://github.com/wurstmineberg/wurstmineberg-common-python.git#egg=wmb'
+    ]
+)
